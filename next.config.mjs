368cb259d5fef29d696dbb22490e21b7fad2be30
Randy Adams <randy@randycadams.com>
2025-11-02 18:54:25 -0800

    Bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,36 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    domains: ['substackcdn.com', 'substack-post-media.s3.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'substackcdn.com',
+        pathname: '/image/fetch/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'substack-post-media.s3.amazonaws.com',
+        pathname: '/public/images/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
   },
 }
 
